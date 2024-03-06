d4de5e3c3c0e6868cb86e2005e1a3d0e248f9f43
Coder.AN <an.hongjun@foxmail.com>
2024-03-06 23:37:41 +0800

    coderan add agent

---
+++ agent/task_room_funcs.py
@@ -0,0 +1,67 @@
+task_room_prompt = """[底层程序消息]接下来你要引导盲人去特定房间。你可以调用的方法有：[{"name": "get_door_info", "description": "获取检测到的门牌信息，以及门牌的方向(左边或右边)。"}, {"name": "move_forward", "description": "向前移动。"}, {"name": "stop", "description": "停止移动。"}, {"name": "det_door", "description": "检测是否有门牌。"}, {"name": "turn_left", "description": "向左旋转90度。"}, {"name": "turn_right", "description": "向右旋转90度。"}, {"name": "pass", "description": "不调用任何方法。"}, {"name": "exit", "description": "结束任务。"}]"""
+
+
+def get_door_info(cli=True):
+    if cli:
+        room = input("------\n现在，由您来充当底层程序，您可以决定InternDog看到的门牌信息。\nInternDog看到的门牌号是？（输入一个三位数或四位数，比如605，1104，1501……）\n[你的输入] >>> ")
+        while True:
+            direct = input("------\n好的，InternDog看到的门牌号是{}。请问他在InternDog的左侧还是右侧？(输入0或1，0代表左侧，1代表右侧。)\n[你的输入] >>> ".format(room))
+            if not direct.isdigit():
+                print("!!!!警告：输入格式错误!!!")
+                continue 
+            direct = int(direct)
+            if direct == 0:
+                direct = "左"
+                break
+            elif direct == 1:
+                direct = "右"
+                break
+            else:
+                print("!!!!警告：输入格式错误!!!")
+                continue
+        message = "[底层程序消息]{}边检测到门牌号{}。".format(direct, room)
+        return message
+
+
+def move_forward(cli=True):
+    if cli:
+        message = "[底层程序消息]开始向前移动。"
+        return message
+
+
+def stop(cli=True):
+    if cli:
+        message = "[底层程序消息]已停止运动。"
+        return message
+
+
+def det_door(cli=True):
+    if cli:
+        while True:
+            answer = input("------\n现在，由您来充当底层程序，您可以决定InternDog是否检测到门牌。\nInternDog检测到门牌了吗？（输入0或1，0代表没看到，1代表看到了。）\n[你的输入] >>> ")
+            if not answer.isdigit():
+                print("!!!!警告：输入格式错误!!!")
+                continue 
+            answer = int(answer)
+            if answer == 0:
+                message = "[底层程序消息]没有检测到门牌信息。"
+                break
+            elif answer == 1:
+                message = "[底层程序消息]检测到门牌信息。"
+                break
+            else:
+                print("!!!!警告：输入格式错误!!!")
+                continue
+        return message
+
+
+def turn_left(cli=True):
+    if cli:
+        message = "[底层程序消息]已向左转向。"
+        return message
+
+
+def turn_right(cli=True):
+    if cli:
+        message = "[底层程序消息]已向左转向。"
+        return message
