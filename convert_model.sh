a2ffc28050f6c6be2f69c128423a58b5f939819c
Coder.AN <an.hongjun@foxmail.com>
2024-03-06 23:37:27 +0800

    coderan add scripts

---
+++ convert_model.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES=2
+rm -rf model/hf
+mkdir model/hf
+export MKL_SERVICE_FORCE_INTEL=1
+export MKL_THREADING_LAYER=GNU
+xtuner convert pth_to_hf ./internlm2_1_8b_qlora_lift_e3.py ./work_dirs/internlm2_1_8b_qlora_lift_e3/iter_27000.pth ./model/hf
---
+++ merge_model.sh
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=2
+rm -rf ./model/minternlm2-chat-1_8b-merged
+xtuner convert merge ./model/Shanghai_AI_Laboratory/internlm2-chat-1_8b-sft ./model/hf ./model/internlm2-chat-1_8b-merged --max-shard-size 2GB
