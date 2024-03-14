0b8046ad7742ceb620030bf430d2b996efc63a7f
Coder.AN <an.hongjun@foxmail.com>
2024-03-14 15:14:23 +0800

    coderan update web_app

--- app.py
+++ app.py
@@ -36,9 +36,41 @@ def common(msg, chat_history, chat_history_clear, prompt, task_id):
     return processor(msg, chat_history, chat_history_clear, task_funcs, prompt, task_id)
 
 
-with gr.Blocks() as demo:
+js = """
+function adjustIframe() {
+    document.getElementById("demo").style.height=document.getElementById("demo").scrollWidth / 16 * 9 +"px";
+}
+"""
+
+with gr.Blocks(js=js) as demo:
     with gr.Row():
-        gr.HTML("""<h1>InternDog</h1>""")
+        gr.Markdown("# InternDog: 基于InternLM2大模型的离线具身智能导盲犬")
+    with gr.Row():
+        with gr.Column(scale=3):
+            gr.HTML("<h3>项目背景</h3>")
+            mk = """     
+            视障人士在日常生活中面临着诸多挑战，其中包括导航困难、安全隐患等。导盲犬的出现可以为他们提供更为便捷、安全的导航方式，有效改善其生活质量。然而，培养一只合格的导盲犬需要花费大量的时间。从幼犬的筛选、基础训练到专业技能的掌握，每一个阶段都需要有专业的训练师、场地和设备以及大量的时间和成本投入，培养一只传统导盲犬的成本可能高达20万元以上。
+
+            四足机器人技术的快速发展使得机器狗代替传统导盲犬成为可能。机器狗导盲犬通过先进的传感器和算法，可以精确感知周围环境并做出智能决策，不受天气、时间或疲劳的限制。它们可以适应各种复杂环境，包括室内、室外、拥挤的城市街道等。开发一套程序可以以近乎零成本的方式迁移到无数台机器狗，使得机器导盲犬的成本相比传统导盲犬大大降低。为此，本团队结合大语言模型技术，开发了一只基于InternLM2大模型的离线具身智能导盲犬。
+            
+            ### 项目简介
+            
+            InternDog使用情景模拟器生成的情景数据作为微调数据集，使用[Xtuner](https://github.com/InternLM/xtuner)工具基于[InternLM2-Chat-1.8B-SFT](https://modelscope.cn/models/Shanghai_AI_Laboratory/internlm2-chat-1_8b-sft/summary)模型进行微调，然后使用本团队开发的[LMDeploy-Jetson](https://github.com/BestAnHongjun/LMDeploy-Jetson)工具对模型进行W4A16量化，在宇树Go1机器狗板载NVIDIA Jetson Xavier NX (8G)上离线部署。
+            
+            > LMDeploy-Jetson: [LMDeploy](https://github.com/InternLM/lmdeploy)在Jetson系列板卡上的移植版本。
+            
+            更多介绍请查看Repo：[[Github]](https://github.com/BestAnHongjun/InternDog)
+            """
+            gr.Markdown(value=mk)
+        with gr.Column(scale=3):
+            gr.HTML("""
+                    <h3>项目演示视频</h3>
+                    <iframe src="//player.bilibili.com/player.html?aid=1701632325&bvid=BV1RK421s7dm&cid=1469102997&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="100%" height="100%" id="demo" onload='document.getElementById("demo").style.height=document.getElementById("demo").scrollWidth / 16 * 9 +"px";'></iframe>""")
+        
+    with gr.Row():
+        gr.HTML("""<hr>
+                <h3>场景模拟器</h3>
+                <p>由于我们的项目涉及真实场景，对于没有机器狗实物的研究者很难复现全部内容验证项目的真实性。为此，我们模拟真实场景，基于OpenXlab平台和Gradio工具制作了一个场景模拟器，由用户来模拟底层控制程序做出相应反应，从而测试大模型的真实表现。</p>""")
     with gr.Row():
         with gr.Tab("您与导盲犬的对话"):
             chatbot_clear = gr.Chatbot(height=500, label="历史消息")
